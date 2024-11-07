5bc27c9b09716544db56d0c42e17a36370d64b2c
Blas092 <blas_092@hotmail.com.ar>
2024-11-07 16:20:23 -0300

    nuevos cambios

--- Assets/script/jugador.cs
+++ Assets/script/jugador.cs
@@ -3,30 +3,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jugador : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed = 1;
-    public float fuerzadesalto=10;
-    private Rigidbody2D rbody;
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    private bool isJumping = false;
+
+    [Header("Ground Check Settings")]
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public LayerMask groundLayer;
+    private bool isGrounded;
+
+    private Rigidbody2D rb;
 
     private void Start()
     {
-        rbody = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        rbody.velocity = new Vector2(Input.GetAxis("Horizontal") * movementSpeed, rbody.velocity.y);
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        Move();
+        CheckGround();
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             Jump();
         }
     }
 
+    private void Move()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        // Voltear el sprite al moverse a la izquierda o derecha
+        if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+    }
+
     private void Jump()
     {
-        rbody.velocity = Vector2.up* fuerzadesalto;
-        //El codigo del salto va aqui
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        isJumping = true;
+    }
+
+    private void CheckGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        if (isGrounded && isJumping)
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
